a10995175ba0ba704909d92828cd44b3b925f52c
unfoundbug <iam@theadmin.co.uk>
2024-01-15 11:41:59 +0000

    Initial commit to Github

---
+++ Packaged/README.md
@@ -0,0 +1,78 @@
+# Vein Booster
+
+![Banner Image]()
+
+## Description
+
+(CHEAT!) Prevents damage to ore voxels by miners and the player. Configurable chance to prevent the damage so you can customise just how much ore gets saved.
+
+-- OLD --
+Adds new technologies to the terraforming tech tree. These technologies dramatically boost the capabilities of the miners. They have a chance of summoning ore from the ether, protecting your valuable voxels. Each level of technology increases this chance by 25%, when all technologies are unlocked, ore voxels will never deplete!
+
+## Screenshots
+
+## Config Options
+
+None.
+
+&nbsp;
+### Techtonica Mod Loader Installation
+
+You can download the Techtonica Mod Loader from here (link not yet available) and use that to install this mod.
+
+### Manual Install Instructions
+
+Note: If you are playing on Gamepass, your game version is likely behind the steam version. Please check the version compatibility chart below.
+
+Your game folder is likely in one of these places:  
+    • Steam: (A-Z):/steam/steamapps/common/Techtonica  
+    • Gamepass: (A-Z):/XboxGames/Techtonica/Content  
+    • Gamepass: Could also be in C:/Program Data/WindowsApps  
+
+1. Download BepInEx v5.4.21 from [here](https://github.com/BepInEx/BepInEx/releases)
+2. Follow the installation instructions [here](https://docs.bepinex.dev/articles/user_guide/installation/index.html)
+3. Extract the contents of the .zip file for this mod.
+4. Drag the "BepInEx" folder into your game folder.
+5. Change config options. 
+
+## compatibility
+| Mod Version | Game Version |
+| --- | --- |
+| 0.0.5 | 0.2.0h |
+| 0.0.4 | 0.2.0h |
+| 0.0.3 | 0.1.1d |
+| 0.0.2 | 0.1.1d |
+| 0.0.1 | 0.1.1d |
+
+## Changelog
+
+### V0.0.5 (Game version 0.2.0)
+
+- Include configuration option for protection chance.
+
+### V0.0.4 (Game version 0.2.0)
+
+Removed research dependancies to reduce crashing issues. 
+
+
+### V0.0.3 (Game version 0.1.1d)
+
+Actually included the up to date readme this time!
+
+### V0.0.2 (Game version 0.1.1d)
+
+Added tech tree dependancies. Not well balanced, but present.
+Including dependanciy on EquinoxsModUtils 
+
+### V0.0.1 (Game version 0.1.1d)
+
+Initial release.
+
+&nbsp;
+## Disclaimer
+
+Note: NEW Games must be loaded, saved, and reloaded for mods to take effect. Existing saves will auto-apply mods. 
+Please be sure to backup your saves before using mods: AppData\LocalLow\Fire Hose Games\Techtonica 
+USE AT YOUR OWN RISK! Techtonica Devs do not provide support for Mods, and cannot recover saves damaged by mod usage.
+
+Some assets may come from Techtonica or from the website created and owned by Fire Hose Games, who hold the copyright of Techtonica. All trademarks and registered trademarks present in any images are proprietary to Fire Hose Games.
---
+++ VeinBooster.cs
@@ -0,0 +1,76 @@
+﻿//#define HIGHLOGGING
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+using Rewired.Integration.UnityUI;
+using UnityEngine.EventSystems;
+using System.Reflection;
+using System.Security.Cryptography;
+using BepInEx.Configuration;
+
+namespace VeinBooster
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class VeinBooster : BaseUnityPlugin
+    {
+        public const string pluginGuid = "veinbooster.nhickling.co.uk";
+        public const string pluginName = "VeinBooster";
+        public const string pluginVersion = "0.0.0.5";
+        private static BepInEx.Logging.ManualLogSource ModLogger;
+        public static ConfigEntry<int> ProtectionChance;
+
+
+        public void Awake()
+        {
+            ModLogger = Logger;
+            ModLogger.LogInfo("VeinBooster: started");
+            Harmony harmony = new Harmony(pluginGuid);
+            ModLogger.LogInfo("VeinBooster: Fetching patch references");
+            MethodInfo original = AccessTools.Method(typeof(PendingVoxelChanges), "TryDig");
+            MethodInfo patch = AccessTools.Method(typeof(VeinBooster), "TryDig_Patch");
+            ModLogger.LogInfo("VeinBooster: Starting Patch");
+            harmony.Patch(original, null, new HarmonyMethod(patch));
+            ModLogger.LogInfo("VeinBooster: Patched");
+            ProtectionChance = ((BaseUnityPlugin)this).Config.Bind<int>("Config", "ProtectionChance", 100, new ConfigDescription("Percentage chance damage to the ore is prevented, reduce this to lower the power of the mod.", (AcceptableValueBase)(object)new AcceptableValueRange<int>(0, 100), Array.Empty<object>()));
+        }
+
+        public static void TryDig_Patch(PendingVoxelChanges __instance, in Vector3Int coord, int digStrength, int miningTier, ref int numResourcesTaken, bool __result)
+        {
+            try
+            {
+                if (!__result)
+                {
+                    // Change happened which did not destroy voxel.
+                    if (numResourcesTaken > 0)
+                    {
+                        if (ShouldRepairVoxel())
+                        {
+                            // De-reference in same manner as original modification. As we know voxel was not broken, but resources aquired, we can shortcut some of the safety checks.
+                            int chunkId = VoxelManager.GetChunkId(coord.x, coord.y, coord.z);
+                            ref ChunkPendingVoxelChanges local1 = ref __instance.chunkData[__instance.GetOrCreateIndexForChunk(in chunkId)];
+                            int indexWithinChunk = local1.GetIndex(coord.x, coord.y, coord.z);
+                            ref ModifiedCoordData local2 = ref local1.GetOrAdd(indexWithinChunk);
+                            local2.integrity += numResourcesTaken;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                ModLogger.LogError($"VeinBooster: TryDig: {ex.Message}");
+
+            }
+        }
+
+        private static bool ShouldRepairVoxel()
+        {
+            return ProtectionChance.Value > UnityEngine.Random.Range(0, 99);
+        }
+    }
+}
